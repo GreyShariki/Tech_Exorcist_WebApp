88136afe5876debd721e7d35dc74e5592c5177c9
Jabiel <maxkazakov441065@gmail.com>
2025-04-30 22:28:59 +0700

    Mb final 1.0.2

--- src/components/formBackend/Acesses.js
+++ src/components/formBackend/Acesses.js
@@ -1,7 +1,11 @@
 export const accessRequest = async (data) => {
   const response = await fetch("http://87.228.82.41:3000/api/addAccess", {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
     body: JSON.stringify({ accessRequest: data }),
   });
   if (!response.ok) throw new Error(await response.text());
@@ -13,7 +17,11 @@ export const notifyAdmins = async (application) => {
     "http://87.228.82.41:3000/api/notify/notify-access",
     {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
       body: JSON.stringify({ application }),
     }
   );
