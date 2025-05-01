9f822f382f81473e1ce64d7f89c5da67a3cfaa1a
Jabiel <maxkazakov441065@gmail.com>
2025-05-01 16:37:56 +0700

    1.1.0

--- src/components/formBackend/Acesses.js
+++ src/components/formBackend/Acesses.js
@@ -1,23 +1,20 @@
 export const accessRequest = async (data) => {
-  const response = await fetch(
-    "https://cors-anywhere.herokuapp.com/https://87.228.82.41:3000/api/addAccess",
-    {
-      method: "POST",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-        "X-Requested-With": "XMLHttpRequest",
-      },
-      body: JSON.stringify({ accessRequest: data }),
-    }
-  );
+  const response = await fetch("http://87.228.82.85:3000/api/addAccess", {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Requested-With": "XMLHttpRequest",
+    },
+    body: JSON.stringify({ accessRequest: data }),
+  });
   if (!response.ok) throw new Error(await response.text());
   return await response.json();
 };
 
 export const notifyAdmins = async (application) => {
   const response = await fetch(
-    "https://cors-anywhere.herokuapp.com/https://87.228.82.41:3000/api/notify/notify-access",
+    "http://87.228.82.85:3000/api/notify/notify-access",
     {
       method: "POST",
       mode: "cors",
